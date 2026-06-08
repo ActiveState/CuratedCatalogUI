6b35a1e79d3af9b3b307e636e6fa92a0b4aa8f1f
martinPavesio <martinp@activestate.com>
2026-06-08 15:39:49 -0300

    fix: version column 8%→11%, sourced 1% from CVE aliases and 2% from package

--- src/pages/ScanReportPage.tsx
+++ src/pages/ScanReportPage.tsx
@@ -99,11 +99,11 @@ export function ScanReportPage() {
             <thead>
               <tr>
                 <th style={{ width: 52 }}>#</th>
-                <th style={{ width: '26%' }}>Package</th>
-                <th style={{ width: '8%' }}>Version</th>
+                <th style={{ width: '24%' }}>Package</th>
+                <th style={{ width: '11%' }}>Version</th>
                 <th style={{ width: '10%' }}>Status</th>
                 <th style={{ width: '12%' }}>Vuln ID</th>
-                <th style={{ width: '12%' }}>CVE Aliases</th>
+                <th style={{ width: '11%' }}>CVE Aliases</th>
                 <th style={{ width: '12%' }}>Fix Version</th>
                 <th style={{ width: '17%' }}>Description</th>
               </tr>
